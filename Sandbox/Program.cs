972f5de48dd169b03490ce547fcafdb72141e79d
Brian Fontana <brian@codefoxtrot.com>
2021-01-11 00:09:59 -0500

    Update Program.cs

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -13,19 +13,21 @@ namespace Sandbox
             var sandbox = new Logger("Sandbox", null, 52428800, 1);
 
             // EXAMPLE: Get all columns.
-            sandbox.Log("\n" + WMIHelper.GetFormattedWMIData(
-                sandbox.LogComponent,
-                "root\\cimv2",
-                "Win32_QuickFixEngineering",
-                null));
+            sandbox.Log("Example: Win32_QuickFixEngineering [ALL COLUMNS]\n" + 
+                WMIHelper.GetFormattedWMIData(
+                    sandbox.LogComponent,
+                    "root\\cimv2",
+                    "Win32_QuickFixEngineering",
+                    null));
 
             // EXAMPLE: Get specified columns.
-            sandbox.Log("\n" + WMIHelper.GetFormattedWMIData(
-                sandbox.LogComponent,
-                "root\\cimv2",
-                "Win32_QuickFixEngineering",
-                new List<string> { "HotFixID", "Description", "InstalledOn", "Caption" },
-                4));
+            sandbox.Log("Example: Win32_QuickFixEngineering [SPECIFIC COLUMNS]\n" + 
+                WMIHelper.GetFormattedWMIData(
+                    sandbox.LogComponent,
+                    "root\\cimv2",
+                    "Win32_QuickFixEngineering",
+                    new List<string> { "HotFixID", "Description", "InstalledOn", "Caption" },
+                    4));
 
             // Close log.
             sandbox.Close();
