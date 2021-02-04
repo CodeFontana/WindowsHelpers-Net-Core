0cb357f0649745768166d0fa5e4f939bac84b3bf
Brian Fontana <brian@codefoxtrot.com>
2021-02-03 22:11:24 -0500

    Add ExtenstionMethods.cs

---
+++ WindowsHelpers/ExtensionMethods.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsLibrary
+{
+    public static class ExtensionMethods
+    {
+        public static string ToDictionaryString<TKey, TValue>(this IDictionary<TKey, TValue> dictionary)
+        {
+            return "{" + string.Join(",", dictionary.Select(kv => kv.Key + "=" + kv.Value).ToArray()) + "}";
+        }
+
+        public static IDictionary<string, string> ToKeyValue(this object metaToken)
+        {
+            if (metaToken == null)
+            {
+                return null;
+            }
+
+            var serializer = new JsonSerializer { ReferenceLoopHandling = ReferenceLoopHandling.Ignore };
+            var token = metaToken as JToken;
+            if (token == null)
+            {
+                // Modified by me: use serializer defined above
+                return ToKeyValue(JObject.FromObject(metaToken, serializer));
+            }
+
+            if (token.HasValues)
+            {
+                var contentData = new Dictionary<string, string>();
+                foreach (var child in token.Children().ToList())
+                {
+                    var childContent = child.ToKeyValue();
+                    if (childContent != null)
+                    {
+                        contentData = contentData.Concat(childContent)
+                                                 .ToDictionary(k => k.Key, v => v.Value);
+                    }
+                }
+
+                return contentData;
+            }
+
+            var jValue = token as JValue;
+            if (jValue?.Value == null)
+            {
+                return null;
+            }
+
+            var value = jValue?.Type == JTokenType.Date ?
+                            jValue?.ToString("o", CultureInfo.InvariantCulture) :
+                            jValue?.ToString(CultureInfo.InvariantCulture);
+
+            return new Dictionary<string, string> { { token.Path, value } };
+        }
+    }
+}
