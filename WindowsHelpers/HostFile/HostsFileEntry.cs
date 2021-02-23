ab806ff269f9cacfca4c74011b557fb6d9397500
Brian Fontana <brian@codefoxtrot.com>
2021-02-23 01:15:25 -0500

    Add foundation for hosts file editting.

---
+++ WindowsHelpers/HostFile/HostsFileEntry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsLibrary.HostFile
+{
+    public class HostsFileEntry
+    {
+        public string Address { get; set; }
+        public List<string> Hosts { get; set; }
+
+        public HostsFileEntry(string address, List<string> hosts)
+        {
+            Address = address;
+            Hosts = hosts;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}={1})", Address, string.Join(",", Hosts));
+        }
+    }
+}
