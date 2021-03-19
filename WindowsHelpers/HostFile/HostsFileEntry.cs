49724d4fee435c08ed0e2cdc25fda03c8a34afe6
Brian Fontana <brian@codefoxtrot.com>
2021-03-19 00:11:22 -0400

    Complete HostsFileHelper functionality

--- WindowsHelpers/HostFile/HostsFileEntry.cs
+++ WindowsHelpers/HostFile/HostsFileEntry.cs
@@ -11,15 +11,72 @@ namespace WindowsLibrary.HostFile
         public string Address { get; set; }
         public List<string> Hosts { get; set; }
 
-        public HostsFileEntry(string address, List<string> hosts)
+        public HostsFileEntry(string address, List<string> hostEntry)
         {
+            if (address == null || ValidateIPv4(address) == false)
+            {
+                throw new ArgumentException($"Invalid IPv4 address [{address}].");
+            }
+
+            if (hostEntry == null ||
+                hostEntry.Count == 0 ||
+                hostEntry.Any(e => e == null || string.IsNullOrWhiteSpace(e)))
+            {
+                throw new ArgumentException($"Invalid host entry [{ string.Join(",", hostEntry ?? new List<string> { "null" }) }].");
+            }
+
             Address = address;
-            Hosts = hosts;
+            Hosts = hostEntry;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var item = obj as HostsFileEntry;
+
+            if (item == null)
+            {
+                return false;
+            }
+
+            if (Address.Equals(item.Address) == false)
+            {
+                return false;
+            }
+
+            if (Hosts.SequenceEqual(item.Hosts) == false)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool ValidateIPv4(string ipString)
+        {
+            if (string.IsNullOrWhiteSpace(ipString))
+            {
+                return false;
+            }
+
+            string[] splitValues = ipString.Split('.');
+
+            if (splitValues.Length != 4)
+            {
+                return false;
+            }
+
+            byte tempForParsing;
+            return splitValues.All(r => byte.TryParse(r, out tempForParsing));
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
         }
 
         public override string ToString()
         {
-            return string.Format("{0}={1})", Address, string.Join(",", Hosts));
+            return string.Format("{0} = {1}", Address, string.Join(",", Hosts));
         }
     }
 }
