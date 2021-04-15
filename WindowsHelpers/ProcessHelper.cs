b11ff5e049d195af0ac760f487cae5a4dd18278a
Brian Fontana <41308769+CodeFontana@users.noreply.github.com>
2021-04-15 01:57:39 -0400

    Update ProcessHelper.cs
    Eliminate unnecessary function that invokes performance counters. Not sure where the need for this method came from.

--- WindowsHelpers/ProcessHelper.cs
+++ WindowsHelpers/ProcessHelper.cs
@@ -192,33 +192,6 @@ namespace WindowsLibrary
             }
         }
 
-        public string GetInstanceNameForProcessId(int processId)
-        {
-            var process = Process.GetProcessById(processId);
-            string processName = Path.GetFileNameWithoutExtension(process.ProcessName);
-            PerformanceCounterCategory cat = new PerformanceCounterCategory("Process");
-
-            // Get all instances that match the process by name.
-            string[] instances = cat.GetInstanceNames()
-                .Where(inst => inst.StartsWith(processName))
-                .ToArray();
-
-            foreach (string instance in instances)
-            {
-                using (PerformanceCounter cnt = new PerformanceCounter("Process", "ID Process", instance, true))
-                {
-                    int val = (int)cnt.RawValue;
-
-                    if (val == processId)
-                    {
-                        return instance;
-                    }
-                }
-            }
-
-            return null;
-        }
-
         public bool IsProcessRunning(string processFriendlyName, bool moreInfo = false)
         {
             processFriendlyName = Path.GetFileNameWithoutExtension(processFriendlyName);
